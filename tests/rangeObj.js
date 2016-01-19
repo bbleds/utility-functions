f744166e3247a555a67dad755ff92f787611926a
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-01-19 15:46:46 -0600

    added some object tests

--- tests/rangeObj.js
+++ tests/rangeObj.js
@@ -1,3 +1,20 @@
-module.exports =  function(){
-    return {};
+module.exports =  function(num){
+    var myObj = {};
+
+    if(num){
+        
+    var rangeMod = require('./range.js');
+
+    var arrayOfNum = rangeMod(num);
+
+    arrayOfNum.map(function(a,b){
+        myObj[a] = a;
+    });
+
+    } else {
+          
+    }
+
+   
+    return myObj;
 }
--- tests/test.js
+++ tests/test.js
@@ -56,11 +56,18 @@ describe('utility functions', function(){
       });
 
       describe('rangeObj', function(){
-        
+           var rangeObj = require('./rangeObj.js');
+
         it('should return an object', function(){
-            var rangeObj = require('./rangeObj.js');
 
             expect(rangeObj()).to.be.an.object;
+        });
+
+        it('should return an object range', function(){
+            expect(rangeObj()).to.eql({});
+            expect(rangeObj(3)).to.eql({0:0, 1:1, 2:2});
+            expect(rangeObj(5)).to.eql({0:0, 1:1, 2:2, 3:3, 4:4});
+
         });
 
 
